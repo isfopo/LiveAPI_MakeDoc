76c4b2df7074d0aec26fefcf8cd0257ebe7dcf05
isfopo <isfopo@users.noreply.github.com>
2024-12-27 14:51:35 +0000

    deploy: f27437304a17fa5909d12ce4e7086ed9670adb19

--- src/helpers/app.py
+++
@@ -1,3 +0,0 @@
-def get_version_number(module) -> str:
-    app = module.Application.get_application()  # get a handle to the App
-    return f"{app.get_major_version()}.{app.get_minor_version()}.{app.get_bugfix_version()}"
--- src/types/BuildMode.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class BuildMode(Enum):
-    Build = "build"
-    Submodule = "submodule"
